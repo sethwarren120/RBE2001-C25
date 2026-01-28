4f94a846e0ae40d7a8127476b5773d97632ca0b9
Seth Warren <sethwarren120@gmail.com>
2026-01-28 14:01:47 -0500

    IK Drive to Point

--- src/robot-nav.cpp
+++ src/robot-nav.cpp
@@ -23,9 +23,8 @@ void Robot::UpdatePose(const Twist& twist)
  */
 void Robot::SetDestination(const Pose& dest)
 {
-    /**
-     * TODO: Turn on LED, as well.
-     */
+    digitalWrite(LED_BUILTIN, HIGH);
+
     Serial.print("Setting dest to: ");
     Serial.print(dest.x);
     Serial.print(", ");
@@ -36,20 +35,18 @@ void Robot::SetDestination(const Pose& dest)
     robotState = ROBOT_DRIVE_TO_POINT;
 }
 
-bool Robot::CheckReachedDestination(void)
-{
-    bool retVal = false;
-    /**
-     * TODO: Add code to check if you've reached destination here.
-     */
-
-    return retVal;
-}
+float drivekP = 1;
+float turnkP = 1;
 
 void Robot::DriveToPoint(void)
 {
     if(robotState == ROBOT_DRIVE_TO_POINT)
     {
+        float errHead = fmod(atan2(destPose.y - currPose.y, destPose.x - currPose.x) - currPose.theta, 360.0) - 180;
+        float errDist = sqrt(pow(destPose.x - currPose.x, 2) + pow(destPose.y - currPose.y, 2)) * cos(errHead);
+
+        float effortLeft = errDist * drivekP - errHead * turnkP;
+        float effortRight = errDist * drivekP + errHead * turnkP;
         /**
          * TODO: Add your IK algorithm here. 
          */
@@ -58,14 +55,19 @@ void Robot::DriveToPoint(void)
         // Print useful stuff here.
 #endif
 
-        /**
-         * TODO: Call chassis.SetMotorEfforts() to command the motion, based on your calculations above.
-         */
+        chassis.SetMotorEfforts(effortLeft, effortRight);
     }
+}
+
+bool Robot::CheckReachedDestination(void)
+{
+    return sqrt(pow(destPose.x - currPose.x, 2) + pow(destPose.y - currPose.y, 2)) < 2.0;
 }
 
 void Robot::HandleDestination(void)
 {
+    chassis.Stop();
+    digitalWrite(LED_BUILTIN, LOW);
     /**
      * TODO: Stop and change state. Turn off LED.
      */
